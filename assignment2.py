20651c10d706f668a3b74d3f5007f4b0e26b6d5d
Austin Baugh <austin@baugh.software>
2021-10-06 13:50:03 -0700

    Finished assignment 2-2

--- assignment2.py
+++ assignment2.py
@@ -34,3 +34,90 @@ for string in strings:
         print("Y")
     else:
         print("N")
+
+print("1)")
+list = [13, "word", 1.5]
+print(list)
+
+print("2)")
+list = [1, 1, [1, 2]]
+print(list[2][1])
+
+print("3)")
+list = ['a', 'b', 'c']
+print(list[1:])
+
+print("4)")
+dict = {
+    'Sunday': 0,
+    'Monday': 1,
+    'Tuesday': 2,
+    'Wednesday': 3,
+    'Thursday': 4,
+    'Friday': 5,
+    'Saturday': 6,
+}
+print(dict)
+
+print("5)")
+dict = {
+    'k1': [1, 2, 3],
+}
+print(dict['k1'][1])
+
+print("6)")
+list = [1, [2, 3]]
+tup = (list[0], list[1][0], list[1][1])
+print(tup)
+
+print("7)")
+string = "Mississippi"
+print(set(string))
+
+print("8)")
+list.append('X')
+print(list)
+
+print("9)")
+print(set([1, 1, 2, 3]))
+
+print("Question 1)")
+result = []
+for i in range(2000, 3200):
+    if i % 7 == 0 and i % 5 != 0:
+        result.append(i)
+print(result)
+
+print("Question 2)")
+def factorial(num):
+    if num == 0:
+        return 1
+    return num * factorial(num - 1)
+num = 8
+print(factorial(num))
+
+print("Question 3)")
+def build_dict(num, result):
+    result[num] = num * num
+    if num == 1:
+        return result
+    return build_dict(num - 1, result)
+print(build_dict(8, {}))
+
+print("Question 4)")
+l=[1,2,3]
+t=tuple(l)
+print(l)
+print(t)
+
+print("Question 5)")
+class Object:
+    def __init__(self):
+        self.s = ""
+    def getString(self, str):
+        self.s = str
+    def printString(self):
+        print(self.s.upper())
+obj = Object()
+obj.getString("some sting")
+obj.printString()
